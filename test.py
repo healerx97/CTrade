9f43d34827574bdaa9f26d9c58d3f03c645c0b47
Kenny Yoon <ghun.yoon@gmail.com>
2022-02-17 03:46:46 -0500

    add sources

--- test.py
+++ test.py
@@ -5,4 +5,7 @@ print(key)
 
 
 
-# pull in candle stats 2days at a time for 
+# pull in candle stats 2days at a time  x 15
+# https://dev.to/edbentley/build-your-react-charts-without-a-library-35o8
+# https://css-tricks.com/how-to-make-charts-with-svg/
+# https://rvlasveld.github.io/blog/2013/07/02/creating-interactive-graphs-with-svg-part-1/
