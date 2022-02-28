fb9edb5097711e9a09f80b785ef97a93a65ca47f
kenny <ghun.yoon@gmail.com>
2022-02-27 22:32:54 -0500

    sample chart

--- frontend/src/Components/Candles.js
+++ frontend/src/Components/Candles.js
@@ -2,31 +2,51 @@ import React, {useEffect} from 'react'
 import axios from 'axios'
 function Candles({currentCoin, candleData, setCandleData}) {
     useEffect(()=> {
-        let coinData = {
-            id: currentCoin.id
-        }
-        //axios gave me 403 errors with cors enabled?
-        // fetch('http://localhost:8000/api/getCandles', {
-        // method: 'POST',
-        // headers: {
-        //     'Content-Type': 'application/json',
-        // },
-        // body: JSON.stringify(coinData),
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //     setCandleData(data)
-        //     console.log(candleData)
-        // })
         console.log(currentCoin)
         console.log(candleData)
     },[])
     
 
   return (
-    <div>
+    <div class='flex flex-col items-center'>
         <div>
             {currentCoin.id}
+        </div>
+        <div class = 'border flex'>
+        <svg viewBox="0 0 500 100" class="chart p-8">
+  
+            <polyline
+                fill="none"
+                stroke="#0074d9"
+                stroke-width="2"
+                points="
+                00,120
+                20,60
+                40,80
+                60,20
+                80,80
+                100,80
+                120,60
+                140,100
+                160,90
+                180,80
+                200, 110
+                220, 10
+                240, 70
+                260, 100
+                280, 100
+                300, 40
+                320, 0
+                340, 100
+                360, 100
+                380, 120
+                400, 60
+                420, 70
+                440, 80
+                "
+            />
+        
+        </svg>
         </div>
     </div>
   )
