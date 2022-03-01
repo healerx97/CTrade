0acdc7597791b58be7db2869a1509dd08cafd087
kenny <ghun.yoon@gmail.com>
2022-03-01 02:45:25 -0500

    candles

--- frontend/src/Components/Candles.js
+++ frontend/src/Components/Candles.js
@@ -1,30 +1,52 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
 function Candles({currentCoin, candleData, setCandleData}) {
+    let w = 1000
+    let h = 1000
+    let minH = 99999
+    let maxH = 0
     useEffect(()=> {
         console.log(currentCoin)
         console.log(candleData)
-    },[])
-    function renderCandles() {
-
+        w = 10 * candleData.length
+        h = w*1.5
         if (candleData) {
-            let w = 10 * candleData.length
-            
-            for (can in candleData) {
-                let time = can['time']
-                let low = can['low']
-                let high = can['high']
-                let open = can['open']
-                let close = can['close']
-                let volume = can['volume']
-                
-
+            for (let c in candleData) {
+                //get min, max height of graph
+                minH = Math.min(minH, c['high'], c['low'])
+                maxH = Math.max(maxH, c['high'], c['low'])   
             }
         }
-        else {
+
+    },[])
+    let renderCandles = candleData? (
+        candleData?.map((can)=>{
+            let time = can['time']
+            let low = can['low']
+            let high = can['high']
+            let open = can['open']
+            let close = can['close']
+            let volume = can['volume']
+            
+            
+            let p1 = candleData.indexOf(candle)*10
+            let v1 = 
+            let v2
+            return <line x1="10" y1="10" x2="0" y2="200" style={{'stroke':'rgb(0,0,0)', 'stroke-width':'20'}} />
+        })
+    ):(
+        <div>No Data</div>
+    )
+    if (candleData) {
+        
+        
+        for (let can in candleData) {
+            
+            
             return (
-                <div>No Data</div>
+                <line x1="10" y1="10" x2="0" y2="200" style={{'stroke':'rgb(0,0,0)', 'stroke-width':'20'}} />
             )
+
         }
     }
 
@@ -34,10 +56,8 @@ function Candles({currentCoin, candleData, setCandleData}) {
             {currentCoin.id}
         </div>
         <div class = 'border flex'>
-        <svg viewBox="-10 -10 210 210" class="chart p-10" vector-effect='non-scaling-stroke'>
-            <line x1="0" y1="0" x2="0" y2="200" style={{'stroke':'rgb(0,0,0)', 'stroke-width':'2'}} />
-            <line x1="10" y1="0" x2="10" y2="200" style={{'stroke':'rgb(0,0,0)', 'stroke-width':'2'}} />
-            <line x1="90" y1="0" x2="90" y2="200" style={{'stroke':'rgb(0,0,0)', 'stroke-width':'2'}} />
+        <svg viewBox= {`-10 -10 ${w} 210`} class="chart p-10" vector-effect='non-scaling-stroke'>
+            {()=> renderCandles()}
         </svg>
         </div>
     </div>
