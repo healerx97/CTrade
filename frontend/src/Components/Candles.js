f4894dfdbbf4761c10ec8df5d4a12423e9cdc772
kenny <ghun.yoon@gmail.com>
2022-03-02 01:46:15 -0500

    need to scale it up/down

--- frontend/src/Components/Candles.js
+++ frontend/src/Components/Candles.js
@@ -2,30 +2,19 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 function Candles({currentCoin, candleData, setCandleData}) {
     let w = 10 * candleData.length
-    let h = w*3
-    let [graphObj, setGraphObj] = useState({minH:99999,maxH:0,d:0})
-    useEffect(()=> {
-        console.log(currentCoin)
-        console.log(candleData) 
-        if (candleData) {
-            for (let c in candleData) {
-                //get min, max height of graph
-                setGraphObj({
-                    minH: Math.min(graphObj['minH'], candleData[c]['low']),
-                    maxH: Math.max(graphObj['maxH'], candleData[c]['high']),
-                    d: graphObj['d']
-                })
-            }
-            setGraphObj({
-                minH: graphObj['minH'],
-                maxH: graphObj['maxH'],
-                d: graphObj['maxH']-graphObj['minH']
-            })
-            console.log(w) 
-        }
-        
-
-    },[])
+    let minH = candleData? (
+        Math.min.apply(Math, candleData.map(function(o) { return o.low }))
+    ): null
+    let maxH = candleData? (
+        Math.max.apply(Math, candleData.map(function(o) { return o.high }))
+    ): null
+    let d = minH? (
+        maxH-minH
+    ): 0
+    let h = d? d:1000
+    
+    console.log(minH)
+    console.log(h)
     let renderCandles = candleData? (
         candleData?.map((can)=>{
             let time = can['time']
@@ -36,8 +25,8 @@ function Candles({currentCoin, candleData, setCandleData}) {
             let volume = can['volume']
             
             let p1 = String(candleData.indexOf(can)*10)
-            let v1 = String((low-graphObj['minH'])/graphObj['d']*h)
-            let v2 = String((high-graphObj['minH'])/graphObj['d']*h)
+            let v1 = String((low-minH)/d*h)
+            let v2 = String((high-minH)/d*h)
             return <line x1={p1} y1={v1} x2={p1} y2={v2} style={{'stroke':'rgb(0,0,0)', 'stroke-width':'1'}} />
         })
     ):(
